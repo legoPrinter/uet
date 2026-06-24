a3b75bb307aead59a7bef4d074a69630e496b76b
LegoPrinter <myrgoroda@icloud.com>
2026-06-23 21:35:23 -0400

    Call canvas object directly from Rust

--- src/iteration1.js
+++ src/iteration1.js
@@ -1,22 +1,9 @@
-import init, {render_frame} from "../pkg/uet.js"
+import init, {start_rust_program} from "../pkg/uet.js"
 
 async function run() {
     await init();
 
-    const canvas = document.querySelector("canvas");
-    const width = 200;
-    const height = 200;
-    canvas.width = width;
-    canvas.height = height;
-    var buffer = render_frame();
-    const image = new ImageData(
-        new Uint8ClampedArray(buffer),
-        width,
-    );
-
-    const ctx = canvas.getContext("2d");
-
-    ctx.putImageData(image, 0, 0);
+    start_rust_program();
 }
 
 run();
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,67 @@
 // Importing the wasm_bindgen crate. Used for Rust - Javascript interaction in a WebAssembly context.
 use wasm_bindgen::prelude::*;
 
-const WIDTH: u32 = 200;
-const HEIGHT: u32 = 200;
+fn log(message: &str) {
+    web_sys::console::log_1(&message.into());
+}
 
 #[wasm_bindgen]
-pub fn render_frame() -> Vec<u8> {
-    let mut buffer = [0u8; (WIDTH * HEIGHT * 4) as usize];
-    let color = [255, 0, 255, 255];
-    for (i, pixel) in buffer.iter_mut().enumerate() {
-        *pixel = color[i % 4];
-    }
-    buffer.to_vec()
+pub fn start_rust_program() {
+    log("Getting window");
+    let window = match web_sys::window() {
+        Some(window) => window,
+        None => {
+            log("Failed to get window, exiting program");
+            return;
+        }
+    };
+    log("Getting document");
+    let document = match window.document() {
+        Some(document) => document,
+        None => {
+            log("Failed to get document, exiting program");
+            return;
+        }
+    };
+    log("Getting canvas");
+    let canvas = match document.query_selector("canvas") {
+        Ok(canvas) => match canvas {
+            Some(canvas) => canvas,
+            None => {
+                log("No canvas element found, exiting program");
+                return;
+            }
+        },
+        Err(_) => {
+            log("Query selector returned an error, exiting program");
+            return;
+        }
+    };
+    log("Converting to canvas element");
+    let canvas = match canvas.dyn_into::<web_sys::HtmlCanvasElement>() {
+        Ok(canvas) => canvas,
+        Err(_) => {
+            log("Failed to convert canvas to HtmlCanvasElement, exiting program");
+            return;
+        }
+    };
+    log("Getting canvas context");
+    let ctx = match canvas.get_context("2d") {
+        Ok(Some(ctx)) => ctx,
+        _ => {
+            log("Failed to get canvas context, exiting program");
+            return;
+        }
+    };
+    log("Converting to context object");
+    let ctx = match ctx.dyn_into::<web_sys::CanvasRenderingContext2d>() {
+        Ok(ctx) => ctx,
+        Err(_) => {
+            log("Failed to convert canvas to 2D context, exiting program");
+            return;
+        }
+    };
+    log("Drawing something on canvas");
+    ctx.set_fill_style_str("blue");
+    ctx.fill_rect(10.0, 10.0, 100.0, 100.0);
 }

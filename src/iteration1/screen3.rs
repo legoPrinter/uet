64a8ab4caa3a0f0430e899d850a101aae2b8141b
LegoPrinter <myrgoroda@icloud.com>
2026-06-24 22:59:01 -0400

    draw animation on third screen

--- src/iteration1/screen3.rs
+++ src/iteration1/screen3.rs
@@ -3,7 +3,20 @@ use super::common::*;
 
 pub fn draw_frame(ctx: &context, input1: f64, input2: f64) -> Result<(), wasm_bindgen::JsValue> {
     white_bg(ctx);
-    ctx.set_fill_style_str(&format!("rgb({}, 255, 0)", input1 * input2 * 255.0));
-    ctx.fill_rect(0.0, 0.0, 200.0, 200.0);
+    
+    let radius = 15.0;
+    let padding = radius;
+    
+    let range = 200.0 - 2.0 * padding;
+
+    let x = padding + input1 * range;
+    let y = padding + input2 * range;
+
+    white_bg(ctx);
+    ctx.set_fill_style_str("black");
+    ctx.begin_path();
+    ctx.arc(x, y, radius, 0.0, 2.0 * PI)?;
+    ctx.fill();
+
     Ok(())
 }
